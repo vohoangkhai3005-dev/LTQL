04229d660f4809a93e2ddfa0897717d2d21953c0
Khai <vohoangkhai3005@gmail.com>
2026-06-27 19:30:52 +0700

    up frmDanhMuc

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmDanhMuc.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTQL
+{
+    public partial class frmDanhMuc : Form
+    {
+        public frmDanhMuc()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDanhMuc_Load(object sender, EventArgs e)
+        {
+            pnlDM.Enabled = false;
+            showDM();
+
+        }
+        //Hàm kiểm tra thông tin
+        private Boolean Kiemtrathongtin()
+        {
+            if(txtMaDM.Text=="")
+            {
+                MessageBox.Show("Bạn chưa nhập mã danh mục", "Thông báo", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (txtTenDM.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập mã danh mục", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+        private void dgvDM_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvDM_Click(object sender, EventArgs e)
+        {
+            //lấy dữ liệu từ lưới lên text box
+            txtMaDM.Text = dgvDM.CurrentRow.Cells["MaDanhMuc"].Value.ToString();
+            txtTenDM.Text = dgvDM.CurrentRow.Cells["TenDanhMuc"].Value.ToString();
+            txtMota.Text = dgvDM.CurrentRow.Cells["Mota"].Value.ToString();
+
+            
+        }
+        private void showDM()
+        {
+            DataTable dtDM = new DataTable();
+            HAMXULY.Connect();
+            string sqlDM = "SELECT * FROM DANHMUC";
+            if (HAMXULY.Truyvan(sqlDM, dtDM))
+            {
+                dgvDM.DataSource = dtDM;
+
+                dgvDM.Columns[0].HeaderText = "Mã danh mục";
+                dgvDM.Columns[0].Width = 200;
+                dgvDM.Columns[1].HeaderText = "Tên danh mục";
+                dgvDM.Columns[1].Width = 250;
+                dgvDM.Columns[2].HeaderText = "Mô tả";
+                dgvDM.Columns[2].Width = 500;
+                dgvDM.DefaultCellStyle.Font = new Font("Segoe UI", 12);
+                dgvDM.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+                dgvDM.RowTemplate.Height = 35;
+                dgvDM.ColumnHeadersHeight = 40;
+                dgvDM.RowsDefaultCellStyle.BackColor = Color.White;
+                dgvDM.AlternatingRowsDefaultCellStyle.BackColor = Color.Gainsboro;
+            }    
+        }
+        private bool dangThem = false;
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            pnlDM.Enabled = true;
+            dangThem = true;
+            btnThem.Enabled = false;
+            btnLuu.Enabled = true;
+            btnXoa.Enabled = false;
+            btnSua.Enabled = false;
+
+            txtMaDM.ResetText();
+            txtTenDM.ResetText();
+            txtMota.ResetText();
+            txtMaDM.Focus();
+        }
+       
+        //xử lý thêm
+        private void ThemDM()
+        {
+            if(Kiemtrathongtin())
+            {
+                HAMXULY.Connect();
+                string sqlInsert = "INSERT INTO DANHMUC(MaDanhMuc, TenDanhMuc, MoTa) " +
+                    "VALUES ('" + txtMaDM.Text +
+                    "','" + txtTenDM.Text +
+                    "','" + txtMota.Text + "')";
+                try
+                {
+                    HAMXULY.RunSQL(sqlInsert);
+                    MessageBox.Show("Bạn đã thêm thành công 1 danh mục: ", txtTenDM.Text);
+                    showDM();
+                }
+                catch (Exception exx)
+                {
+                    MessageBox.Show(exx.Message);
+                }
+            }    
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            pnlDM.Enabled = true;
+            dangThem = false;
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
+            btnSua.Enabled = false;
+
+            txtMaDM.Enabled = false;
+            txtTenDM.SelectAll();
+        }
+        private void SuaDM()
+        {
+            if(Kiemtrathongtin())
+            {
+                HAMXULY.Connect();
+                string sqlUpdate = "UPDATE DANHMUC" +
+                    " SET TenDanhMuc = N'" + txtTenDM.Text + "'," +
+                    "MoTa = N'" + txtMota.Text + "'" +
+                    "WHERE MaDanhMuc = N'" + txtMaDM.Text + "' ";
+                try
+                {
+                    HAMXULY.RunSQL(sqlUpdate);
+                    MessageBox.Show("Bạn đã sửa thành công 1 danh mục", "Thông báo");
+                    showDM();
+                }
+                catch (Exception exx)
+                {
+                    MessageBox.Show(exx.Message);
+                }
+            }    
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (!Kiemtrathongtin())
+            return;
+            if (dangThem)
+            {
+                ThemDM();
+                btnThem.Enabled = true;
+                btnSua.Enabled = true;
+                btnXoa.Enabled = true;
+            }
+            else
+            {
+                SuaDM();
+            }
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            pnlDM.Enabled = false;
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            XoaDM();
+        }
+        //xử lý nút xóa
+        private void XoaDM()
+        {
+            if (MessageBox.Show("Bạn có muốn xóa danh mục này không?",
+                    "Thông báo",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+                string sqlDelete = "DELETE FROM DANHMUC WHERE MaDanhMuc = N'" + txtMaDM.Text + "'";
+                HAMXULY.RunSQL(sqlDelete);
+                showDM();   //load lại dữ liệu
+                MessageBox.Show("Bạn đã xóa thành công danh mục");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmDanhMuc.designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace LTQL
+{
+    partial class frmDanhMuc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDM = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMota = new System.Windows.Forms.TextBox();
+            this.txtMaDM = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlDM = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.txtTenDM = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDM)).BeginInit();
+            this.pnlDM.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDM
+            // 
+            this.dgvDM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDM.Location = new System.Drawing.Point(59, 446);
+            this.dgvDM.Name = "dgvDM";
+            this.dgvDM.Size = new System.Drawing.Size(804, 239);
+            this.dgvDM.TabIndex = 35;
+            this.dgvDM.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDM_CellContentClick);
+            this.dgvDM.Click += new System.EventHandler(this.dgvDM_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(88, 355);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 24);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "THAO TÁC";
+            // 
+            // txtMota
+            // 
+            this.txtMota.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMota.Location = new System.Drawing.Point(205, 226);
+            this.txtMota.Margin = new System.Windows.Forms.Padding(6);
+            this.txtMota.Name = "txtMota";
+            this.txtMota.Size = new System.Drawing.Size(334, 29);
+            this.txtMota.TabIndex = 29;
+            // 
+            // txtMaDM
+            // 
+            this.txtMaDM.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaDM.Location = new System.Drawing.Point(205, 103);
+            this.txtMaDM.Margin = new System.Windows.Forms.Padding(6);
+            this.txtMaDM.Name = "txtMaDM";
+            this.txtMaDM.Size = new System.Drawing.Size(334, 29);
+            this.txtMaDM.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 226);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 24);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Mô tả";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 165);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 24);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Tên danh mục";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 103);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 24);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Mã danh mục";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 24);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "THÔNG TIN DANH MỤC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, -1);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 31);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Quản lý danh mục sản phẩm";
+            // 
+            // pnlDM
+            // 
+            this.pnlDM.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlDM.Controls.Add(this.txtTenDM);
+            this.pnlDM.Controls.Add(this.txtMaDM);
+            this.pnlDM.Controls.Add(this.label3);
+            this.pnlDM.Controls.Add(this.label5);
+            this.pnlDM.Controls.Add(this.txtMota);
+            this.pnlDM.Controls.Add(this.label4);
+            this.pnlDM.Location = new System.Drawing.Point(59, 46);
+            this.pnlDM.Margin = new System.Windows.Forms.Padding(6);
+            this.pnlDM.Name = "pnlDM";
+            this.pnlDM.Size = new System.Drawing.Size(804, 290);
+            this.pnlDM.TabIndex = 32;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnThoat);
+            this.panel2.Controls.Add(this.btnThem);
+            this.panel2.Controls.Add(this.btnHuy);
+            this.panel2.Controls.Add(this.btnSua);
+            this.panel2.Controls.Add(this.btnLuu);
+            this.panel2.Controls.Add(this.btnXoa);
+            this.panel2.Location = new System.Drawing.Point(59, 349);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(804, 91);
+            this.panel2.TabIndex = 34;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(676, 33);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(110, 43);
+            this.btnThoat.TabIndex = 18;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(15, 33);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(110, 43);
+            this.btnThem.TabIndex = 13;
+            this.btnThem.Text = "➕ Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(542, 33);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(110, 43);
+            this.btnHuy.TabIndex = 17;
+            this.btnHuy.Text = "🚫 Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(145, 33);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(110, 43);
+            this.btnSua.TabIndex = 14;
+            this.btnSua.Text = "✏️ Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(411, 33);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(110, 43);
+            this.btnLuu.TabIndex = 16;
+            this.btnLuu.Text = "💾 Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(281, 33);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(110, 43);
+            this.btnXoa.TabIndex = 15;
+            this.btnXoa.Text = "🗑️ Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // txtTenDM
+            // 
+            this.txtTenDM.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenDM.Location = new System.Drawing.Point(205, 165);
+            this.txtTenDM.Name = "txtTenDM";
+            this.txtTenDM.Size = new System.Drawing.Size(334, 29);
+            this.txtTenDM.TabIndex = 30;
+            // 
+            // frmDanhMuc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 735);
+            this.Controls.Add(this.dgvDM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlDM);
+            this.Controls.Add(this.panel2);
+            this.Name = "frmDanhMuc";
+            this.Text = "frmDanhMuc";
+            this.Load += new System.EventHandler(this.frmDanhMuc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDM)).EndInit();
+            this.pnlDM.ResumeLayout(false);
+            this.pnlDM.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDM;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMota;
+        private System.Windows.Forms.TextBox txtMaDM;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlDM;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.TextBox txtTenDM;
+    }
+}
