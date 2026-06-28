96686a0e0ef81ac93adfbfa2b4327e48a7c65658
vothanhnghia123 <vothanhnghia1503@gmail.com>
2026-06-28 14:37:20 +0700

    báo cáo và sql hd thiếu manv đã sửa

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmBaoCao.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmBaoCao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dtpTuNgay = new System.Windows.Forms.DateTimePicker();
+            this.dtpDenNgay = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.lblTongDoanhThu = new System.Windows.Forms.Label();
+            this.dgvDoanhThu = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtpTuNgayTop = new System.Windows.Forms.DateTimePicker();
+            this.dtpDenNgayTop = new System.Windows.Forms.DateTimePicker();
+            this.cboTop = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnXemTop = new System.Windows.Forms.Button();
+            this.dgvTopSanPham = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTopSanPham)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BÁO CÁO ";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(853, 507);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgvDoanhThu);
+            this.tabPage1.Controls.Add(this.lblTongDoanhThu);
+            this.tabPage1.Controls.Add(this.btnXem);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.dtpDenNgay);
+            this.tabPage1.Controls.Add(this.dtpTuNgay);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 38);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(845, 465);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Doanh thu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvTopSanPham);
+            this.tabPage2.Controls.Add(this.btnXemTop);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.cboTop);
+            this.tabPage2.Controls.Add(this.dtpDenNgayTop);
+            this.tabPage2.Controls.Add(this.dtpTuNgayTop);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Location = new System.Drawing.Point(4, 38);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(845, 465);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Top sản phẩm";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Loại thống kê";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(33, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(148, 33);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Theo ngày";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(226, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(156, 33);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Theo tháng";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // dtpTuNgay
+            // 
+            this.dtpTuNgay.Location = new System.Drawing.Point(152, 121);
+            this.dtpTuNgay.Name = "dtpTuNgay";
+            this.dtpTuNgay.Size = new System.Drawing.Size(200, 34);
+            this.dtpTuNgay.TabIndex = 3;
+            // 
+            // dtpDenNgay
+            // 
+            this.dtpDenNgay.Location = new System.Drawing.Point(537, 125);
+            this.dtpDenNgay.Name = "dtpDenNgay";
+            this.dtpDenNgay.Size = new System.Drawing.Size(200, 34);
+            this.dtpDenNgay.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Từ ngày ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Đến ngày ";
+            // 
+            // btnXem
+            // 
+            this.btnXem.Location = new System.Drawing.Point(318, 197);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(183, 37);
+            this.btnXem.TabIndex = 7;
+            this.btnXem.Text = "Xem báo cáo";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // lblTongDoanhThu
+            // 
+            this.lblTongDoanhThu.AutoSize = true;
+            this.lblTongDoanhThu.Location = new System.Drawing.Point(33, 264);
+            this.lblTongDoanhThu.Name = "lblTongDoanhThu";
+            this.lblTongDoanhThu.Size = new System.Drawing.Size(187, 29);
+            this.lblTongDoanhThu.TabIndex = 8;
+            this.lblTongDoanhThu.Text = "Tổng doanh thu:";
+            // 
+            // dgvDoanhThu
+            // 
+            this.dgvDoanhThu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDoanhThu.Location = new System.Drawing.Point(23, 325);
+            this.dgvDoanhThu.Name = "dgvDoanhThu";
+            this.dgvDoanhThu.RowTemplate.Height = 24;
+            this.dgvDoanhThu.Size = new System.Drawing.Size(806, 128);
+            this.dgvDoanhThu.TabIndex = 9;
+            this.dgvDoanhThu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDoanhThu_CellContentClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(390, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Đến ngày ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 29);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Từ ngày ";
+            // 
+            // dtpTuNgayTop
+            // 
+            this.dtpTuNgayTop.Location = new System.Drawing.Point(139, 42);
+            this.dtpTuNgayTop.Name = "dtpTuNgayTop";
+            this.dtpTuNgayTop.Size = new System.Drawing.Size(200, 34);
+            this.dtpTuNgayTop.TabIndex = 9;
+            // 
+            // dtpDenNgayTop
+            // 
+            this.dtpDenNgayTop.Location = new System.Drawing.Point(516, 42);
+            this.dtpDenNgayTop.Name = "dtpDenNgayTop";
+            this.dtpDenNgayTop.Size = new System.Drawing.Size(200, 34);
+            this.dtpDenNgayTop.TabIndex = 10;
+            // 
+            // cboTop
+            // 
+            this.cboTop.FormattingEnabled = true;
+            this.cboTop.Location = new System.Drawing.Point(32, 156);
+            this.cboTop.Name = "cboTop";
+            this.cboTop.Size = new System.Drawing.Size(121, 37);
+            this.cboTop.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 29);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Top ";
+            // 
+            // btnXemTop
+            // 
+            this.btnXemTop.Location = new System.Drawing.Point(318, 224);
+            this.btnXemTop.Name = "btnXemTop";
+            this.btnXemTop.Size = new System.Drawing.Size(121, 55);
+            this.btnXemTop.TabIndex = 13;
+            this.btnXemTop.Text = "Xem";
+            this.btnXemTop.UseVisualStyleBackColor = true;
+            this.btnXemTop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dgvTopSanPham
+            // 
+            this.dgvTopSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTopSanPham.Location = new System.Drawing.Point(16, 321);
+            this.dgvTopSanPham.Name = "dgvTopSanPham";
+            this.dgvTopSanPham.RowTemplate.Height = 24;
+            this.dgvTopSanPham.Size = new System.Drawing.Size(815, 131);
+            this.dgvTopSanPham.TabIndex = 14;
+            // 
+            // frmBaoCao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 648);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmBaoCao";
+            this.Text = "frmBaoCao";
+            this.Load += new System.EventHandler(this.frmBaoCao_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTopSanPham)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgvDoanhThu;
+        private System.Windows.Forms.Label lblTongDoanhThu;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDenNgay;
+        private System.Windows.Forms.DateTimePicker dtpTuNgay;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvTopSanPham;
+        private System.Windows.Forms.Button btnXemTop;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cboTop;
+        private System.Windows.Forms.DateTimePicker dtpDenNgayTop;
+        private System.Windows.Forms.DateTimePicker dtpTuNgayTop;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmBaoCao.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmBaoCao : Form
+    {
+        public frmBaoCao()
+        {
+            InitializeComponent();
+        }
+        private void ShowDoanhThu()
+        {
+            DataTable dt = new DataTable();
+
+            HAMXULY.Connect();
+
+            string sql =
+                    @"SELECT HD.MAHD,
+                             HD.NGAYLAP,
+                             NV.TENNV,
+                             KH.TENKH,
+                             HD.TONGTIEN
+                    FROM HOADON HD
+                    INNER JOIN NHANVIEN NV
+                    ON HD.MANV = NV.MANV
+                    INNER JOIN KHACHHANG KH
+                    ON HD.MAKH = KH.MAKH";
+
+            if (HAMXULY.Truyvan(sql, dt))
+            {
+                dgvDoanhThu.DataSource = dt;
+
+                dgvDoanhThu.Columns[0].HeaderText = "Mã HĐ";
+                dgvDoanhThu.Columns[1].HeaderText = "Ngày lập";
+                dgvDoanhThu.Columns[2].HeaderText = "Nhân viên";
+                dgvDoanhThu.Columns[3].HeaderText = "Khách hàng";
+                dgvDoanhThu.Columns[4].HeaderText = "Tổng tiền";
+
+                dgvDoanhThu.EnableHeadersVisualStyles = false;
+                dgvDoanhThu.ColumnHeadersDefaultCellStyle.BackColor = Color.Cyan;
+
+                TinhTongDoanhThu();
+            }
+        }
+        private void dgvDoanhThu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            HAMXULY.Connect();
+
+            string sql =
+                @"SELECT HD.MAHD,
+                         HD.NGAYLAP,
+                         NV.TENNV,
+                         KH.TENKH,
+                         HD.TONGTIEN
+                FROM HOADON HD
+                INNER JOIN NHANVIEN NV
+                ON HD.MANV = NV.MANV
+                INNER JOIN KHACHHANG KH
+                ON HD.MAKH = KH.MAKH
+                WHERE HD.NGAYLAP BETWEEN '" +
+                dtpTuNgay.Value.ToString("yyyy-MM-dd") +
+                "' AND '" +
+                dtpDenNgay.Value.ToString("yyyy-MM-dd") + "'";
+
+            if (HAMXULY.Truyvan(sql, dt))
+            {
+                dgvDoanhThu.DataSource = dt;
+                TinhTongDoanhThu();
+
+            }
+        }
+        private void TinhTongDoanhThu()
+        {
+            decimal tong = 0;
+
+            foreach (DataGridViewRow r in dgvDoanhThu.Rows)
+            {
+                if (r.Cells["TONGTIEN"].Value != null &&
+                    r.Cells["TONGTIEN"].Value != DBNull.Value)
+                {
+                    tong += Convert.ToDecimal(r.Cells["TONGTIEN"].Value);
+                }
+            }
+
+            lblTongDoanhThu.Text = "Tổng doanh thu: " +
+                                   tong.ToString("N0") + " VNĐ";
+        }
+
+        private void frmBaoCao_Load(object sender, EventArgs e)
+        {
+            HAMXULY.Connect();
+
+            cboTop.Items.Add("5");
+            cboTop.Items.Add("10");
+            cboTop.Items.Add("20");
+
+            cboTop.SelectedIndex = 0;
+
+            ShowDoanhThu();
+            ShowTopSanPham();
+        }
+        private void ShowTopSanPham()
+        {
+            DataTable dt = new DataTable();
+
+            HAMXULY.Connect();
+
+            string sql =
+            @"SELECT TOP 5
+        SP.MASP,
+        SP.TENSP,
+        SUM(CT.SOLUONG) AS SOLUONGBAN,
+        SUM(CT.THANHTIEN) AS DOANHTHU
+      FROM CHITIETHOADON CT
+      INNER JOIN SANPHAM SP
+      ON CT.MASP = SP.MASP
+      GROUP BY SP.MASP, SP.TENSP
+      ORDER BY SOLUONGBAN DESC";
+
+            if (HAMXULY.Truyvan(sql, dt))
+            {
+                dgvTopSanPham.DataSource = dt;
+
+                dgvTopSanPham.Columns[0].HeaderText = "Mã SP";
+                dgvTopSanPham.Columns[1].HeaderText = "Tên sản phẩm";
+                dgvTopSanPham.Columns[2].HeaderText = "Số lượng bán";
+                dgvTopSanPham.Columns[3].HeaderText = "Doanh thu";
+
+                dgvTopSanPham.EnableHeadersVisualStyles = false;
+                dgvTopSanPham.ColumnHeadersDefaultCellStyle.BackColor = Color.Cyan;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            HAMXULY.Connect();
+
+            string sql =
+            @"SELECT TOP " + cboTop.Text + @"
+        SP.MASP,
+        SP.TENSP,
+        SUM(CT.SOLUONG) AS SOLUONGBAN,
+        SUM(CT.THANHTIEN) AS DOANHTHU
+      FROM CHITIETHOADON CT
+      INNER JOIN SANPHAM SP
+      ON CT.MASP = SP.MASP
+      INNER JOIN HOADON HD
+      ON CT.MAHD = HD.MAHD
+      WHERE HD.NGAYLAP BETWEEN '" +
+              dtpTuNgayTop.Value.ToString("yyyy-MM-dd") +
+              "' AND '" +
+              dtpDenNgayTop.Value.ToString("yyyy-MM-dd") +
+              @"'
+      GROUP BY SP.MASP, SP.TENSP
+      ORDER BY SOLUONGBAN DESC";
+
+            if (HAMXULY.Truyvan(sql, dt))
+            {
+                dgvTopSanPham.DataSource = dt;
+            }
+        }
+    }
+}
