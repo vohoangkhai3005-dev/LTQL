b8a4fde4d35e1158775aabd52ebaae2941dd12c0
tranbang1101 <trbang1101@gmail.com>
2026-07-01 10:15:54 +0700

    Add files via upload

---
+++ DACK/frmHoaDon.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace DACK
+{
+    partial class frmHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpNgayLap = new System.Windows.Forms.DateTimePicker();
+            this.cmbKhachHang = new System.Windows.Forms.ComboBox();
+            this.txtMaHD = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.btnLamMoi = new System.Windows.Forms.Button();
+            this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.pnlDoanhThu = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSoHoaDon = new System.Windows.Forms.Label();
+            this.lblDoanhThu = new System.Windows.Forms.Label();
+            this.qlbhDataSet1 = new DACK.qlbhDataSet1();
+            this.hOADONBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.hOADONTableAdapter = new DACK.qlbhDataSet1TableAdapters.HOADONTableAdapter();
+            this.mAHDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mAKHDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nGAYLAPDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tONGTIENDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+            this.pnlDoanhThu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.qlbhDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hOADONBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLamMoi);
+            this.panel1.Controls.Add(this.btnTim);
+            this.panel1.Controls.Add(this.txtMaHD);
+            this.panel1.Controls.Add(this.cmbKhachHang);
+            this.panel1.Controls.Add(this.dtpNgayLap);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 206);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ngày lập hóa đơn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Khách hàng:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(396, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã hóa đơn";
+            // 
+            // dtpNgayLap
+            // 
+            this.dtpNgayLap.Location = new System.Drawing.Point(174, 12);
+            this.dtpNgayLap.Name = "dtpNgayLap";
+            this.dtpNgayLap.Size = new System.Drawing.Size(200, 30);
+            this.dtpNgayLap.TabIndex = 3;
+            // 
+            // cmbKhachHang
+            // 
+            this.cmbKhachHang.FormattingEnabled = true;
+            this.cmbKhachHang.Location = new System.Drawing.Point(135, 82);
+            this.cmbKhachHang.Name = "cmbKhachHang";
+            this.cmbKhachHang.Size = new System.Drawing.Size(198, 30);
+            this.cmbKhachHang.TabIndex = 4;
+            // 
+            // txtMaHD
+            // 
+            this.txtMaHD.Location = new System.Drawing.Point(506, 82);
+            this.txtMaHD.Name = "txtMaHD";
+            this.txtMaHD.Size = new System.Drawing.Size(165, 30);
+            this.txtMaHD.TabIndex = 5;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(150, 139);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(95, 38);
+            this.btnTim.TabIndex = 6;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // btnLamMoi
+            // 
+            this.btnLamMoi.Location = new System.Drawing.Point(444, 139);
+            this.btnLamMoi.Name = "btnLamMoi";
+            this.btnLamMoi.Size = new System.Drawing.Size(95, 38);
+            this.btnLamMoi.TabIndex = 7;
+            this.btnLamMoi.Text = "Làm mới";
+            this.btnLamMoi.UseVisualStyleBackColor = true;
+            this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click);
+            // 
+            // dgvHoaDon
+            // 
+            this.dgvHoaDon.AutoGenerateColumns = false;
+            this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHoaDon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.mAHDDataGridViewTextBoxColumn,
+            this.mAKHDataGridViewTextBoxColumn,
+            this.nGAYLAPDataGridViewTextBoxColumn,
+            this.tONGTIENDataGridViewTextBoxColumn});
+            this.dgvHoaDon.DataSource = this.hOADONBindingSource;
+            this.dgvHoaDon.Location = new System.Drawing.Point(35, 250);
+            this.dgvHoaDon.Name = "dgvHoaDon";
+            this.dgvHoaDon.RowHeadersWidth = 51;
+            this.dgvHoaDon.RowTemplate.Height = 24;
+            this.dgvHoaDon.Size = new System.Drawing.Size(649, 157);
+            this.dgvHoaDon.TabIndex = 1;
+            this.dgvHoaDon.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHoaDon_CellDoubleClick);
+            // 
+            // pnlDoanhThu
+            // 
+            this.pnlDoanhThu.Controls.Add(this.lblDoanhThu);
+            this.pnlDoanhThu.Controls.Add(this.lblSoHoaDon);
+            this.pnlDoanhThu.Controls.Add(this.label5);
+            this.pnlDoanhThu.Controls.Add(this.label4);
+            this.pnlDoanhThu.Location = new System.Drawing.Point(13, 436);
+            this.pnlDoanhThu.Name = "pnlDoanhThu";
+            this.pnlDoanhThu.Size = new System.Drawing.Size(700, 74);
+            this.pnlDoanhThu.TabIndex = 2;
+            this.pnlDoanhThu.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tổng số hóa đơn:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(364, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 22);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tổng doanh thu:";
+            // 
+            // lblSoHoaDon
+            // 
+            this.lblSoHoaDon.AutoSize = true;
+            this.lblSoHoaDon.Location = new System.Drawing.Point(175, 20);
+            this.lblSoHoaDon.Name = "lblSoHoaDon";
+            this.lblSoHoaDon.Size = new System.Drawing.Size(60, 22);
+            this.lblSoHoaDon.TabIndex = 2;
+            this.lblSoHoaDon.Text = "label6";
+            // 
+            // lblDoanhThu
+            // 
+            this.lblDoanhThu.AutoSize = true;
+            this.lblDoanhThu.Location = new System.Drawing.Point(506, 20);
+            this.lblDoanhThu.Name = "lblDoanhThu";
+            this.lblDoanhThu.Size = new System.Drawing.Size(60, 22);
+            this.lblDoanhThu.TabIndex = 3;
+            this.lblDoanhThu.Text = "label7";
+            // 
+            // qlbhDataSet1
+            // 
+            this.qlbhDataSet1.DataSetName = "qlbhDataSet1";
+            this.qlbhDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hOADONBindingSource
+            // 
+            this.hOADONBindingSource.DataMember = "HOADON";
+            this.hOADONBindingSource.DataSource = this.qlbhDataSet1;
+            // 
+            // hOADONTableAdapter
+            // 
+            this.hOADONTableAdapter.ClearBeforeFill = true;
+            // 
+            // mAHDDataGridViewTextBoxColumn
+            // 
+            this.mAHDDataGridViewTextBoxColumn.DataPropertyName = "MAHD";
+            this.mAHDDataGridViewTextBoxColumn.HeaderText = "MAHD";
+            this.mAHDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.mAHDDataGridViewTextBoxColumn.Name = "mAHDDataGridViewTextBoxColumn";
+            this.mAHDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // mAKHDataGridViewTextBoxColumn
+            // 
+            this.mAKHDataGridViewTextBoxColumn.DataPropertyName = "MAKH";
+            this.mAKHDataGridViewTextBoxColumn.HeaderText = "MAKH";
+            this.mAKHDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.mAKHDataGridViewTextBoxColumn.Name = "mAKHDataGridViewTextBoxColumn";
+            this.mAKHDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nGAYLAPDataGridViewTextBoxColumn
+            // 
+            this.nGAYLAPDataGridViewTextBoxColumn.DataPropertyName = "NGAYLAP";
+            this.nGAYLAPDataGridViewTextBoxColumn.HeaderText = "NGAYLAP";
+            this.nGAYLAPDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nGAYLAPDataGridViewTextBoxColumn.Name = "nGAYLAPDataGridViewTextBoxColumn";
+            this.nGAYLAPDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // tONGTIENDataGridViewTextBoxColumn
+            // 
+            this.tONGTIENDataGridViewTextBoxColumn.DataPropertyName = "TONGTIEN";
+            this.tONGTIENDataGridViewTextBoxColumn.HeaderText = "TONGTIEN";
+            this.tONGTIENDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tONGTIENDataGridViewTextBoxColumn.Name = "tONGTIENDataGridViewTextBoxColumn";
+            this.tONGTIENDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // frmHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 530);
+            this.Controls.Add(this.pnlDoanhThu);
+            this.Controls.Add(this.dgvHoaDon);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmHoaDon";
+            this.Text = "Hóa đơn";
+            this.Load += new System.EventHandler(this.frmHoaDon_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+            this.pnlDoanhThu.ResumeLayout(false);
+            this.pnlDoanhThu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.qlbhDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hOADONBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLamMoi;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtMaHD;
+        private System.Windows.Forms.ComboBox cmbKhachHang;
+        private System.Windows.Forms.DateTimePicker dtpNgayLap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvHoaDon;
+        private System.Windows.Forms.Panel pnlDoanhThu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDoanhThu;
+        private System.Windows.Forms.Label lblSoHoaDon;
+        private System.Windows.Forms.Label label5;
+        private qlbhDataSet1 qlbhDataSet1;
+        private System.Windows.Forms.BindingSource hOADONBindingSource;
+        private qlbhDataSet1TableAdapters.HOADONTableAdapter hOADONTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mAHDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mAKHDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nGAYLAPDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tONGTIENDataGridViewTextBoxColumn;
+    }
+}
---
+++ DACK/frmHoaDon.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DACK
+{
+    public partial class frmHoaDon : Form
+    {
+        string connectionString = @"Data Source=.;Initial Catalog=QLBH;Integrated Security=True";
+
+        public frmHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmHoaDon_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qlbhDataSet1.HOADON' table. You can move, or remove it, as needed.
+            this.hOADONTableAdapter.Fill(this.qlbhDataSet1.HOADON);
+            dtpNgayLap.Value = DateTime.Now;
+            LoadComboKhachHang();
+            LoadDanhSachHoaDon();
+        }
+
+        private void LoadComboKhachHang()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT MaKH, TenKH FROM KhachHang ORDER BY TenKH";
+                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                DataRow row = dt.NewRow();
+                row["MaKH"] = DBNull.Value;
+                row["TenKH"] = "Tất cả khách hàng";
+                dt.Rows.InsertAt(row, 0);
+
+                cmbKhachHang.DataSource = dt;
+                cmbKhachHang.DisplayMember = "TenKH";
+                cmbKhachHang.ValueMember = "MaKH";
+            }
+        }
+
+        private void LoadDanhSachHoaDon()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sql = @"
+                    SELECT 
+                        h.MaHD as [Mã HD],
+                        h.NgayLap as [Ngày lập],
+                        ISNULL(k.TenKH, N'Khách lẻ') as [Khách hàng],
+                        h.NhanVien as [Nhân viên],
+                        h.TongTien as [Tổng tiền],
+                        h.GiamGia as [Giảm giá],
+                        h.ThanhTien as [Thành tiền]
+                    FROM HoaDon h
+                    LEFT JOIN KhachHang k ON h.MaKH = k.MaKH
+                    WHERE CAST(h.NgayLap AS DATE) = CAST(@ngayLap AS DATE)
+                      AND (@maKH IS NULL OR h.MaKH = @maKH)
+                      AND (@maHD = '' OR CAST(h.MaHD AS NVARCHAR) LIKE '%' + @maHD + '%')
+                    ORDER BY h.MaHD DESC";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ngayLap", dtpNgayLap.Value.Date);
+                cmd.Parameters.AddWithValue("@maKH", cmbKhachHang.SelectedValue ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@maHD", txtMaHD.Text.Trim());
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvHoaDon.DataSource = dt;
+
+                // Định dạng cột tiền
+                if (dgvHoaDon.Columns["Tổng tiền"] != null)
+                    dgvHoaDon.Columns["Tổng tiền"].DefaultCellStyle.Format = "N0";
+                if (dgvHoaDon.Columns["Giảm giá"] != null)
+                    dgvHoaDon.Columns["Giảm giá"].DefaultCellStyle.Format = "N0";
+                if (dgvHoaDon.Columns["Thành tiền"] != null)
+                    dgvHoaDon.Columns["Thành tiền"].DefaultCellStyle.Format = "N0";
+            }
+
+            TinhThongKe();
+        }
+
+        private void TinhThongKe()
+        {
+            decimal tongDoanhThu = 0;
+            foreach (DataGridViewRow row in dgvHoaDon.Rows)
+            {
+                if (row.Cells["Thành tiền"].Value != null)
+                    tongDoanhThu += Convert.ToDecimal(row.Cells["Thành tiền"].Value);
+            }
+
+            lblSoHoaDon.Text = $"Tổng số hóa đơn: {dgvHoaDon.Rows.Count}";
+            lblDoanhThu.Text = $"Tổng doanh thu: {tongDoanhThu:N0} ₫";
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            LoadDanhSachHoaDon();
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            txtMaHD.Clear();
+            cmbKhachHang.SelectedIndex = 0;
+            dtpNgayLap.Value = DateTime.Now;
+            LoadDanhSachHoaDon();
+        }
+
+        private void dgvHoaDon_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Double click vào dòng để xem / in hóa đơn
+            if (e.RowIndex >= 0)
+            {
+                int maHD = Convert.ToInt32(dgvHoaDon.Rows[e.RowIndex].Cells["Mã HD"].Value);
+
+                frmInHoaDon f = new frmInHoaDon(maHD);
+                f.ShowDialog();
+            }
+        }
+    }
+}
